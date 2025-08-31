b0a5e82b1d1ac7f7ce62254ae2c2c356638f3ae5
Ben Eng <whysguy52@gmail.com>
2025-08-31 16:15:32 -0500

    failed attempt
    failed attempts to dynamically separate resources

--- assets/ship_parts/base_module/base_module_script.gd
+++ assets/ship_parts/base_module/base_module_script.gd
@@ -7,5 +7,5 @@ func set_local_texture():
   var totalTexturePath = currentDir.get_current_dir() + "/" + fileList[0]
   var textImage = load(totalTexturePath)
   var text_resource = get_node("base_module").get_node("base_module").get_node("Cube").get_active_material(0)
-  #text_resource.albedo_texture.resource_local_to_scene = true
+  text_resource.albedo_texture.resource_local_to_scene = true
   text_resource.set("albedo_texture", textImage)
--- assets/ship_parts/storage_module/storage_module.gd
+++ assets/ship_parts/storage_module/storage_module.gd
@@ -3,11 +3,13 @@ extends "res://assets/ship_parts/base_module/base_module_script.gd"
 
 
 func _ready():
-  set_local_texture()
-  return
+  #set_local_texture()
+  #return
 
   var scene = load("res://assets/ship_parts/base_module/base_module.tscn")
   var scene_dup = scene.duplicate(true)
+  scene_dup.resource_local_to_scene = true
   var local_instance = scene_dup.instantiate()
   add_child(local_instance)
+  print(scene_dup.resource_local_to_scene)
   set_local_texture()
